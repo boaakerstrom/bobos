9750434de2ba3aee0bae9daf8a043a16b76f3129
boaakerstrom <noreply>
2014-05-27 09:36:54 +0000

    Add component bobo-navigation

---
+++ ui/bobo-navigation.reel/bobo-navigation.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/bobo-navigation.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class BoboNavigation
+ * @extends Component
+ */
+exports.BoboNavigation = Component.specialize(/** @lends BoboNavigation# */ {
+    constructor: {
+        value: function BoboNavigation() {
+            this.super();
+        }
+    }
+});
